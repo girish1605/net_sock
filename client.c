1e6ea323838b4a323d3a77ebdc3f8730c54fdf4f
Girish Bhangale <girish.1605@gmail.com>
2016-11-10 12:11:15 +0000

    Initial commit

---
+++ client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 6000
+#define BUFFSIZE 1024
+#define CHECK_ERR(v, msg) \
+	if(v){ perror(msg); exit(EXIT_FAILURE); } \
+	else perror(msg);
+
+int main(int argc, char* argv[]) {
+
+	int sockfd, retval;
+	struct sockaddr_in servAddr;
+	socklen_t servLen;
+	//char buff[BUFFSIZE];
+	char buff;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	CHECK_ERR((sockfd < 0), "Socket");
+	
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(PORT);
+	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servLen = sizeof(servAddr);
+
+	retval = connect(sockfd, (struct sockaddr *)&servAddr, servLen);
+	CHECK_ERR((retval < 0), "Connet");
+
+	do {
+		//memset(buff, '\0', strlen(buff));
+		retval = recv(sockfd, &buff, sizeof(char), 0);
+		CHECK_ERR((retval < 0),"Read");
+		printf("CPU :: %c", buff);
+		
+		//memset(buff, '\0', strlen(buff));
+		printf("Me :: ");
+		scanf("%c", &buff);
+		retval = send(sockfd, &buff, sizeof(char), 0);
+		CHECK_ERR((retval < 0), "Write");
+	} while (1);
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 6000
+#define BACKLOG 5
+#define BUFFSIZE 1024
+
+#define CHECK_ERR(v, msg) \
+	if(v){ perror(msg); exit(EXIT_FAILURE); } \
+	else perror(msg);
+
+#define handle_error(msg) \
+	do{ perror(msg); exit(EXIT_FAILURE); }while(0);
+
+#define success(msg) { perror(msg); }
+
+int main(int argc, char** argv) {
+	int sockfd, retval, connfd;
+	struct sockaddr_in servAddr, clientAddr;
+	socklen_t servLen, clientLen;
+	//char buff[BUFFSIZE];
+	char buff;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	CHECK_ERR((sockfd < 0), "Socket");
+	
+	bzero(&servAddr, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(PORT);
+	servAddr.sin_addr.s_addr = INADDR_ANY;
+	servLen = sizeof(servAddr);
+
+	retval = bind(sockfd, (struct sockaddr *)&servAddr, servLen);
+	CHECK_ERR((retval < 0),"Bind");
+
+	retval = listen(sockfd, BACKLOG);
+	CHECK_ERR((retval < 0),"Listen");
+
+	do {
+		retval = accept(sockfd, (struct sockaddr *)&clientAddr, &clientLen);
+		CHECK_ERR((retval < 0), "Accept");
+		printf("Yey! +1 client connected\n");		
+		do {
+			//memset(buff, '\0', strlen(buff));
+			printf("Me :: ");
+			scanf(" %c", &buff);
+			retval = send(sockfd, &buff, sizeof(char), 0);
+			CHECK_ERR((retval < 0), "Write");
+			
+			//memset(buff, '\0', strlen(buff));
+			retval = recv(sockfd, &buff, sizeof(char), 0);	
+			CHECK_ERR((retval < 0),"Read");
+			printf("User :: %c", buff);
+		} while(1);	
+
+	} while(1);
+	
+	return 0;
+}
+
